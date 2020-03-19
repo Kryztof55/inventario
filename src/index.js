236ddfc8e7b689912493e6b6c6cb796a8de564fc
kryztofFarango <kryztof.dg@gmail.com>
2020-03-19 16:19:07 -0600

    Estructura de front

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+
+import '../node_modules/materialize-css/dist/css/materialize.min.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
